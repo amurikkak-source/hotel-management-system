44b62029797de47f5233cb0fef3dc09e44a783ef
amurikkak-source <amurikkak@gmail.com>
2026-03-05 03:53:35 +0500

    Create RoomDAO.java file

---
+++ src/main/java/com/hotel/dao/RoomDAO.java
@@ -0,0 +1,24 @@
+package com.hotel.dao;
+
+public class RoomDAO {
+    // Method to add a room
+    public void addRoom(Room room) {
+        // Implementation to add room to database
+    }
+
+    // Method to get a room by id
+    public Room getRoomById(int id) {
+        // Implementation to get room from database
+        return null;
+    }
+
+    // Method to update room details
+    public void updateRoom(Room room) {
+        // Implementation to update room in database
+    }
+
+    // Method to delete a room
+    public void deleteRoom(int id) {
+        // Implementation to delete room from database
+    }
+}
